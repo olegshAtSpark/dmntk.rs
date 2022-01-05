ac52f5e0dfdca5e74928823b3e2b4dff187e7aee
Dariusz Depta <dariusz.depta@dmntk.io>
2022-01-05 11:19:22 +0100

    Added URIs.

--- model/src/model/mod.rs
+++ model/src/model/mod.rs
@@ -44,7 +44,7 @@ use dmntk_feel::{FeelType, Name};
 use std::convert::TryFrom;
 use std::fmt::Display;
 
-pub const URI_FEEL: &str = "https://www.omg.org/spec/DMN/20191 111/FEEL/";
+pub const URI_FEEL: &str = "https://www.omg.org/spec/DMN/20191111/FEEL/";
 pub const URI_UNINTERPRETED: &str = "http://www.omg.org/spec/DMN/uninterpreted/20140801";
 pub const URI_XML_SCHEMA: &str = "http://www.w3.org/2001/XMLSchema";
 
