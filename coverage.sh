37329dba444053ce0752095e9b45112c5878781c
Dariusz Depta <dariusz.depta@dmntk.io>
2022-01-19 14:00:53 +0100

    Updated coverage script.

--- coverage.sh
+++ coverage.sh
@@ -6,7 +6,12 @@ export CARGO_INCREMENTAL=0
 export RUSTDOCFLAGS="-Cpanic=abort"
 export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort"
 
+# run all tests
 cargo test
+
+# run building feel-parser with features, after reformatting, no changes in source code are expected
+cargo build -p dmntk-feel-parser --features=parsing-tables
+cargo fmt -p dmntk-feel-parser
 
 mkdir ./target/lcov
 mkdir ./target/coverage
