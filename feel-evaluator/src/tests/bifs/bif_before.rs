b610f2126624788aac354125336ae9a5e9e2b40f
Dariusz Depta <dariusz.depta@dmntk.io>
2022-02-01 21:43:14 +0100

    Added tests.

--- feel-evaluator/src/tests/bifs/bif_before.rs
+++ feel-evaluator/src/tests/bifs/bif_before.rs
@@ -40,115 +40,241 @@ fn _0001() {
 
 #[test]
 fn _0002() {
-  te_bool(false, &scope!(), r#"before(10,1)"#, false);
+  te_bool(false, &scope!(), r#"before(date("2022-01-31"),date("2022-02-01"))"#, true);
 }
 
 #[test]
 fn _0003() {
-  te_bool(false, &scope!(), r#"before(10,10)"#, false);
+  te_bool(false, &scope!(), r#"before(10,1)"#, false);
 }
 
 #[test]
 fn _0004() {
-  te_bool(false, &scope!(), r#"before(1,[1..10])"#, false);
+  te_bool(false, &scope!(), r#"before(date("2022-02-01"),date("2022-01-31"))"#, false);
 }
 
 #[test]
 fn _0005() {
-  te_bool(false, &scope!(), r#"before(1,(1..10])"#, true);
+  te_bool(false, &scope!(), r#"before(10,10)"#, false);
 }
 
 #[test]
 fn _0006() {
-  te_bool(false, &scope!(), "before(1,[5..10])", true);
+  te_bool(false, &scope!(), r#"before(date("1981-12-13"),date("1981-12-13"))"#, false);
 }
 
 #[test]
 fn _0007() {
-  te_bool(false, &scope!(), r#"before([1..10],10)"#, false);
+  te_bool(false, &scope!(), r#"before(1,[1..10])"#, false);
 }
 
 #[test]
 fn _0008() {
-  te_bool(false, &scope!(), r#"before([1..10),10)"#, true);
+  te_bool(
+    false,
+    &scope!(),
+    r#"before(date("1960-11-15"),[date("1960-11-15")..date("1960-12-31")])"#,
+    false,
+  );
 }
 
 #[test]
 fn _0009() {
-  te_bool(false, &scope!(), r#"before([1..10],15)"#, true);
+  te_bool(false, &scope!(), r#"before(1,(1..10])"#, true);
 }
 
 #[test]
 fn _0010() {
-  te_bool(false, &scope!(), r#"before([1..10],[15..20])"#, true);
+  te_bool(false, &scope!(), r#"before(date("1960-11-15"),(date("1960-11-15")..date("1960-12-31")])"#, true);
 }
 
 #[test]
 fn _0011() {
-  te_bool(false, &scope!(), r#"before([1..10],[10..20])"#, false);
+  te_bool(false, &scope!(), "before(1,[5..10])", true);
 }
 
 #[test]
 fn _0012() {
-  te_bool(false, &scope!(), r#"before([1..10),[10..20])"#, true);
+  te_bool(false, &scope!(), r#"before(date("1960-11-15"),[date("1960-11-17")..date("1960-12-31")])"#, true);
 }
 
 #[test]
 fn _0013() {
-  te_bool(false, &scope!(), r#"before([1..10],(10..20])"#, true);
+  te_bool(false, &scope!(), r#"before([1..10],10)"#, false);
 }
 
 #[test]
 fn _0014() {
-  te_bool(false, &scope!(), r#"before([1..10),(10..20])"#, true);
+  te_bool(
+    false,
+    &scope!(),
+    r#"before([date("1960-11-17")..date("1960-12-31")],date("1960-12-31"))"#,
+    false,
+  );
 }
 
 #[test]
 fn _0015() {
-  te_bool(false, &scope!(), r#"before(point1:1,point2:10)"#, true);
+  te_bool(false, &scope!(), r#"before([1..10),10)"#, true);
 }
+
 #[test]
 fn _0016() {
-  te_bool(false, &scope!(), r#"before(point2:10,point1:1)"#, true);
+  te_bool(false, &scope!(), r#"before([date("1960-11-17")..date("1960-12-31")),date("1960-12-31"))"#, true);
 }
 
 #[test]
 fn _0017() {
-  te_bool(false, &scope!(), r#"before(point1:10,point2:1)"#, false);
+  te_bool(false, &scope!(), r#"before([1..10],15)"#, true);
 }
 
 #[test]
 fn _0018() {
-  te_bool(false, &scope!(), r#"before(point1:10,point2:10)"#, false);
+  te_bool(false, &scope!(), r#"before([date("1960-11-17")..date("1960-12-31")],date("1961-02-12"))"#, true);
 }
 
 #[test]
 fn _0019() {
-  te_bool(false, &scope!(), r#"before(range1:[1..10),range2:(10..20])"#, true);
+  te_bool(false, &scope!(), r#"before([1..10],[15..20])"#, true);
 }
 
 #[test]
 fn _0020() {
-  te_bool(false, &scope!(), r#"before(point:1,range:(1..10])"#, true);
+  te_bool(
+    false,
+    &scope!(),
+    r#"before([date("1960-11-17")..date("1960-12-31")],[date("1961-01-01")..date("1960-05-31")])"#,
+    true,
+  );
 }
 
 #[test]
 fn _0021() {
-  te_bool(false, &scope!(), r#"before(range:(1..10],point:1)"#, true);
+  te_bool(false, &scope!(), r#"before([1..10],[10..20])"#, false);
 }
 
 #[test]
 fn _0022() {
-  te_null(false, &scope!(), r#"before()"#, r#"expected 2 parameters, actual number of parameters is 0"#);
+  te_bool(
+    false,
+    &scope!(),
+    r#"before([date("1960-11-17")..date("1960-12-31")],[date("1960-12-31")..date("1960-05-31")])"#,
+    false,
+  );
 }
 
 #[test]
 fn _0023() {
-  te_null(false, &scope!(), r#"before(1)"#, r#"expected 2 parameters, actual number of parameters is 1"#);
+  te_bool(false, &scope!(), r#"before([1..10),[10..20])"#, true);
 }
 
 #[test]
 fn _0024() {
+  te_bool(
+    false,
+    &scope!(),
+    r#"before([date("1960-11-17")..date("1960-12-31")),[date("1960-12-31")..date("1960-05-31")])"#,
+    true,
+  );
+}
+
+#[test]
+fn _0025() {
+  te_bool(false, &scope!(), r#"before([1..10],(10..20])"#, true);
+}
+
+#[test]
+fn _0026() {
+  te_bool(
+    false,
+    &scope!(),
+    r#"before([date("1960-11-17")..date("1960-12-31")],(date("1960-12-31")..date("1960-05-31")])"#,
+    true,
+  );
+}
+
+#[test]
+fn _0027() {
+  te_bool(false, &scope!(), r#"before([1..10),(10..20])"#, true);
+}
+
+#[test]
+fn _0028() {
+  te_bool(
+    false,
+    &scope!(),
+    r#"before([date("1960-11-17")..date("1960-12-31")),(date("1960-12-31")..date("1960-05-31")])"#,
+    true,
+  );
+}
+
+#[test]
+fn _0029() {
+  te_bool(false, &scope!(), r#"before(point1:1,point2:10)"#, true);
+}
+
+#[test]
+fn _0030() {
+  te_bool(false, &scope!(), r#"before(point1:date("2022-01-31"),point2:date("2022-02-01"))"#, true);
+}
+
+#[test]
+fn _0031() {
+  te_bool(false, &scope!(), r#"before(point2:10,point1:1)"#, true);
+}
+
+#[test]
+fn _0032() {
+  te_bool(false, &scope!(), r#"before(point2:date("2022-02-01"),point1:date("2022-01-31"))"#, true);
+}
+
+#[test]
+fn _0033() {
+  te_bool(false, &scope!(), r#"before(point1:10,point2:1)"#, false);
+}
+
+#[test]
+fn _0034() {
+  te_bool(false, &scope!(), r#"before(point1:date("2022-02-01"),point2:date("2022-01-31"))"#, false);
+}
+
+#[test]
+fn _0035() {
+  te_bool(false, &scope!(), r#"before(point1:10,point2:10)"#, false);
+}
+
+#[test]
+fn _0036() {
+  te_bool(false, &scope!(), r#"before(point1:date("2022-02-01"),point2:date("2022-02-01"))"#, false);
+}
+
+#[test]
+fn _0037() {
+  te_bool(false, &scope!(), r#"before(range1:[1..10),range2:(10..20])"#, true);
+}
+
+#[test]
+fn _0038() {
+  te_bool(false, &scope!(), r#"before(point:1,range:(1..10])"#, true);
+}
+
+#[test]
+fn _0039() {
+  te_bool(false, &scope!(), r#"before(range:(1..10],point:1)"#, true);
+}
+
+#[test]
+fn _0040() {
+  te_null(false, &scope!(), r#"before()"#, r#"expected 2 parameters, actual number of parameters is 0"#);
+}
+
+#[test]
+fn _0041() {
+  te_null(false, &scope!(), r#"before(1)"#, r#"expected 2 parameters, actual number of parameters is 1"#);
+}
+
+#[test]
+fn _0042() {
   te_null(
     false,
     &scope!(),
@@ -158,12 +284,12 @@ fn _0024() {
 }
 
 #[test]
-fn _0025() {
+fn _0043() {
   te_null(false, &scope!(), r#"before(p1: 1, point2: 2)"#, r#"[named::before] invalid named parameters"#);
 }
 
 #[test]
-fn _0026() {
+fn _0044() {
   te_null(
     false,
     &scope!(),
@@ -173,17 +299,12 @@ fn _0026() {
 }
 
 #[test]
-fn _0027() {
-  te_bool(false, &scope!(), r#"before(date("2022-01-31"),date("2022-02-01"))"#, true);
-}
-
-#[test]
-fn _0028() {
+fn _0045() {
   te_bool(false, &scope!(), r#"before(date("2022-01-31"),[date("2022-02-01")..date("2022-12-31")])"#, true);
 }
 
 #[test]
-fn _0029() {
+fn _0046() {
   te_bool(
     false,
     &scope!(),
